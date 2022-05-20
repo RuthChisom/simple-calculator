a02c4a471637b9700d88f664436630cd159af373
Ruth Chisom <ndukwe100805043@gmail.com>
2022-05-20 13:55:50 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+function calculate(num1, operator, num2){
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    switch(operator){
+        case '+' : return num1 + num2;  //addition
+        case '-' : return num1 - num2;  //subtraction
+        case '*' : return num1 * num2;  //multiplication
+        case '/' : return num1 / num2;  //division
+        case '%' : return num1 % num2;  //modulus
+        default : return "Error";
+    }
+}
+firstNumber = 50;
+SecondNumber = 4;
+console.log("The first number is",firstNumber);
+console.log("The second number is",SecondNumber);
+console.log("+ gives",calculate(firstNumber, '+', SecondNumber));
+console.log("- gives",calculate(firstNumber, '-', SecondNumber));
+console.log("* gives",calculate(firstNumber, '*', SecondNumber));
+console.log("/ gives",calculate(firstNumber, '/', SecondNumber));
+console.log("% gives",calculate(firstNumber, '%', SecondNumber));
+console.log("& gives",calculate(firstNumber, '&', SecondNumber));
+// console.log("50 + 4 = ",calculate('50', '+', 4));
+// console.log("50 - 4 = ",calculate('50', '-', 4));
+// console.log("50 * 4 = ",calculate('50', '*', 4));
+// console.log("50 / 4 = ",calculate(50, '/', 4));
+// console.log("50 % 4 = ",calculate(50, '%', 4));
+// console.log("50 & 4 = ",calculate(50, '&', 4));
